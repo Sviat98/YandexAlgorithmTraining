b4b2b051b92fd35e9d263b299892d3c23171edc9
Sviat98 <bashkevich.98@gmail.com>
2024-03-22 00:43:17 +0300

    block 3 - solved I - Football, but got a PE during check (=

--- src/main/kotlin/season_5/block_3/I_Football.kt
+++ src/main/kotlin/season_5/block_3/I_Football.kt
@@ -1,31 +1,34 @@
 package org.example.season_5.block_3
 
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+
 fun football() {
     val matchRegex = Regex("\"([A-Z]|[a-z]|\\s)+\"\\s-\\s\"([A-Z]|[a-z]|\\s)+\"\\s(\\d){1,2}:(\\d){1,2}")
 
     val teamRegex = Regex("\"([A-Z]|[a-z]|\\s)+\"")
 
-    val scoreRegex = Regex("(\\d){1,2}:(\\d){1,2}")
+    var output = ""
 
-    val goalRegex = Regex("([A-Z]|[a-z]|\\s)+\\s(\\d){1,2}'")
+    val scoreRegex = Regex("(\\d){1,2}:(\\d){1,2}")
 
     val scorerRegex = Regex("([A-Z]|[a-z]|\\s)+")
 
-    val scoreMinuteRegex = Regex("(\\d){1,2}'")
+    val scoreMinuteRegex = Regex("(\\d){1,2}")
+
+    val goalRegex = Regex("$scoreRegex\\s$scoreMinuteRegex'")
 
     val teamsMap = mutableMapOf<String, Pair<Int, MutableSet<String>>>()
 
     val playersMap = mutableMapOf<String, Pair<Int, MutableList<Int>>>()
 
-    while (true) {
-        val footballInfo = readln().trim()
+    val footballFacts =
+        File("input.txt").inputStream().readBytes().toString(Charsets.UTF_8).split("\r\n").filter { it.isNotEmpty() }
 
-//        println(footballInfo)
-
+    footballFacts.forEachIndexed { index, footballInfo ->
         when {
             footballInfo.matches(matchRegex) -> {
-                var firstMinuteToScore = 90
-
                 var firstTeamFirstScorer = Pair("", 0)
                 var secondTeamFirstScorer = Pair("", 0)
 
@@ -42,7 +45,7 @@ fun football() {
 
                 val secondTeam = teams.elementAt(1)
 
-                val secondTeamInfo = teamsMap[firstTeam] ?: Pair(0, mutableSetOf())
+                val secondTeamInfo = teamsMap[secondTeam] ?: Pair(0, mutableSetOf())
 
                 val secondTeamGamesAmount = secondTeamInfo.first + 1
 
@@ -51,8 +54,8 @@ fun football() {
                 val (firstTeamScore, secondTeamScore) = scoreRegex.find(footballInfo)!!.value.split(':')
                     .map { it.toInt() }
 
-                repeat(firstTeamScore) {
-                    val goal = readln()
+                repeat(firstTeamScore) { goalIndex ->
+                    val goal = footballFacts[index + goalIndex + 1]
 
                     val scorer = scorerRegex.find(goal)!!.value.trim()
 
@@ -60,7 +63,7 @@ fun football() {
 
                     val scorerInfo = playersMap[scorer] ?: Pair(0, mutableListOf())
 
-                    val scoringMinute = scoreMinuteRegex.find(goal)!!.value.trim().dropLast(1).toInt()
+                    val scoringMinute = scoreMinuteRegex.find(goal)!!.value.trim().toInt()
 
                     val scoringMinutes = scorerInfo.second
 
@@ -68,13 +71,12 @@ fun football() {
 
                     playersMap[scorer] = Pair(scorerInfo.first, scoringMinutes)
 
-                    if (it == 0) {
+                    if (goalIndex == 0) {
                         firstTeamFirstScorer = Pair(scorer, scoringMinute)
                     }
                 }
-
-                repeat(secondTeamScore) {
-                    val goal = readln()
+                repeat(secondTeamScore) { goalIndex ->
+                    val goal = footballFacts[index + goalIndex + firstTeamScore + 1]
 
                     val scorer = scorerRegex.find(goal)!!.value.trim()
 
@@ -82,7 +84,7 @@ fun football() {
 
                     val scorerInfo = playersMap[scorer] ?: Pair(0, mutableListOf())
 
-                    val scoringMinute = scoreMinuteRegex.find(goal)!!.value.trim().dropLast(1).toInt()
+                    val scoringMinute = scoreMinuteRegex.find(goal)!!.value.trim().toInt()
 
                     val scoringMinutes = scorerInfo.second
 
@@ -90,7 +92,7 @@ fun football() {
 
                     playersMap[scorer] = Pair(scorerInfo.first, scoringMinutes)
 
-                    if (it == 0) {
+                    if (goalIndex == 0) {
                         secondTeamFirstScorer = Pair(scorer, scoringMinute)
                     }
                 }
@@ -98,19 +100,20 @@ fun football() {
                 val firstPlayerToScoreInMatch =
                     listOf(firstTeamFirstScorer, secondTeamFirstScorer).minBy { it.second }.first
 
-                val firstPlayerToScoreInMatchInfo = playersMap[firstPlayerToScoreInMatch] ?: Pair(0, mutableListOf())
+                if(firstPlayerToScoreInMatch.isNotEmpty()){
+                    val firstPlayerToScoreInMatchInfo = playersMap[firstPlayerToScoreInMatch] ?: Pair(0, mutableListOf())
 
-                val playerFirstToScoreTimes =
-                    firstPlayerToScoreInMatchInfo.first + 1 //количество раз, когда игрок открывал счет в матче
+                    val playerFirstToScoreTimes =
+                        firstPlayerToScoreInMatchInfo.first + 1 //количество раз, когда игрок открывал счет в матче
 
-                playersMap[firstPlayerToScoreInMatch] =
-                    Pair(playerFirstToScoreTimes, firstPlayerToScoreInMatchInfo.second)
-
+                    playersMap[firstPlayerToScoreInMatch] =
+                        Pair(playerFirstToScoreTimes, firstPlayerToScoreInMatchInfo.second)
+                }
                 teamsMap[firstTeam] = Pair(firstTeamGamesAmount, firstTeamSquad)
 
                 teamsMap[secondTeam] = Pair(secondTeamGamesAmount, secondTeamSquad)
-
             }
+
             footballInfo.startsWith("Total goals for") -> {
                 val team = teamRegex.find(footballInfo)!!.value.trim().drop(1).dropLast(1)
 
@@ -123,16 +126,118 @@ fun football() {
 
                     totalTeamGoals += scoringMinutesCount
                 }
-                println(totalTeamGoals)
+                //println(totalTeamGoals)
+                output+=totalTeamGoals.toString()+"\r\n"
             }
+
             footballInfo.startsWith("Total goals by") -> {
-                val player = footballInfo.trim().substring(16, footballInfo.length)
+                val player = footballInfo.substring(16, footballInfo.length)
 
                 val playerTotalGoals = teamsMap[player]?.second?.size ?: 0
 
-                println(playerTotalGoals)
+               // println(playerTotalGoals)
+                output+=playerTotalGoals.toString()+"\r\n"
+
             }
+            footballInfo.startsWith("Mean goals per game for") -> {
+                val team = teamRegex.find(footballInfo)!!.value.trim().drop(1).dropLast(1)
+
+                var totalTeamGoals = 0
+
+                val teamTotalMatches = teamsMap[team]?.first ?: 0
+
+                val teamScorers = teamsMap[team]?.second
+
+                teamScorers?.forEach { teamScorer ->
+                    val scoringMinutesCount = playersMap[teamScorer]?.second?.size ?: 0
+
+                    totalTeamGoals += scoringMinutesCount
+                }
+
+                val teamGoalsPerGame = (totalTeamGoals / teamTotalMatches).toDouble()
+
+                //println(teamGoalsPerGame)
+                output+=teamGoalsPerGame.toString()+"\r\n"
+
+            }
+            footballInfo.startsWith("Mean goals per game by") -> {
+                val player = footballInfo.substring(23, footballInfo.length)
+
+                val totalPlayerMatches =
+                    teamsMap.filterValues { player in it.second }.entries.map { it.value.first }.firstOrNull() ?: 0
+
+                val playerTotalGoals = playersMap[player]?.second?.size ?: 0
+
+                val playerGoalsPerGame =
+                    if (totalPlayerMatches == 0) 0.0 else playerTotalGoals.toDouble() / totalPlayerMatches
+
+                //println(playerGoalsPerGame)
+                output+=playerGoalsPerGame.toString()+"\r\n"
+            }
+
+            footballInfo.matches(Regex("Goals on minute $scoreMinuteRegex by $scorerRegex")) -> {
+                val scoringMinute = scoreMinuteRegex.find(footballInfo)!!.value.toInt()
+
+                val scorer = footballInfo.substring(footballInfo.indexOf(" by ") + 4)
+
+                val goalsOnThatMinuteScored = playersMap[scorer]?.second?.filter { it == scoringMinute }?.size ?: 0
+
+                //println(goalsOnThatMinuteScored)
+                output+=goalsOnThatMinuteScored.toString()+"\r\n"
+
+            }
+
+            footballInfo.matches(Regex("Goals on first $scoreMinuteRegex minutes by $scorerRegex")) -> {
+                val scoringMinute = scoreMinuteRegex.find(footballInfo)!!.value.toInt()
+
+                val scorer = footballInfo.substring(footballInfo.indexOf(" by ") + 4)
+
+                val goalsOnFirstMinutesScored = playersMap[scorer]?.second?.filter { it <= scoringMinute }?.size ?: 0
+
+                //println(goalsOnFirstMinutesScored)
+                output+=goalsOnFirstMinutesScored.toString()+"\r\n"
+            }
+
+            footballInfo.matches(Regex("Goals on last $scoreMinuteRegex minutes by $scorerRegex")) -> {
+                val scoringMinute = 91 - scoreMinuteRegex.find(footballInfo)!!.value.toInt()
+
+                val scorer = footballInfo.substring(footballInfo.indexOf(" by ") + 4)
+
+                val goalsOnLastMinutesScored = playersMap[scorer]?.second?.filter { it >= scoringMinute }?.size ?: 0
+
+                //println(goalsOnLastMinutesScored)
+                output+=goalsOnLastMinutesScored.toString()+"\r\n"
+            }
+
+            footballInfo.startsWith("Score opens by") -> {
+                val teamOrScorer = footballInfo.substring(15)
+
+                var scoreOpenAmount = 0
+
+                if (teamOrScorer[0] == '"') {//team
+
+                    val team = teamOrScorer.drop(1).dropLast(1)
+                    val teamSquad = teamsMap[team]?.second
+
+                    teamSquad?.forEach { player ->
+                        scoreOpenAmount += playersMap[player]?.first ?: 0
+                    }
+
+                } else {//scorer
+                    val scorer = teamOrScorer
+
+                    scoreOpenAmount = playersMap[scorer]?.first ?: 0
+                }
+                //println(scoreOpenAmount)
+                output+=scoreOpenAmount.toString()+"\r\n"
+            }
+            else -> Unit
         }
     }
 
+    val outputStream = BufferedWriter(FileWriter("output.txt"))
+    outputStream.write(output)
+    outputStream.flush()
+
+    //File("output.txt").writeText(output)
 }
