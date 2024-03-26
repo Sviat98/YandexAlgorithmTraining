d3ff391ede95d78a9d9bd0c9f35e3b673a0f1d13
Sviat98 <bashkevich.98@gmail.com>
2024-03-26 19:28:11 +0300

    block 4 - solved A and B

--- src/main/kotlin/season_5/block_4/A_ArraySearch.kt
+++ src/main/kotlin/season_5/block_4/A_ArraySearch.kt
@@ -7,49 +7,58 @@ fun arraySearch() {
 
     val answers = mutableListOf<Int>()
 
-
     val queries = readln().toInt()
 
     val begin = System.currentTimeMillis()
 
-
-
     repeat(queries) {
-//        println(it)
 
         val (minNumber, maxNumber) = readln().split(' ').map { it.toInt() }
 
-        var leftIndex = 0
-        var rightIndex = numbers.size - 1
+        var excludedNumbers = 0
 
-        var entriesExcluded = 0
+        if (numbers.size == 1) {
+            excludedNumbers = if (numbers[0] in minNumber..maxNumber) 0 else 1
+        } else {
+            var leftIndex = 0
+            var rightIndex = numbers.size - 1
 
-        while (leftIndex < rightIndex) {
-            val middleIndex = (leftIndex + rightIndex) / 2
+            // находим максимальный элемент, который меньше минимума
+            while (leftIndex < rightIndex) {
+                val middleIndex = (leftIndex + rightIndex + 1) / 2
 
-            if (numbers[middleIndex] >= minNumber) {
-                rightIndex = middleIndex
-            } else {
-                leftIndex = middleIndex + 1
-                entriesExcluded++
+                if (numbers[middleIndex] < minNumber) {
+                    leftIndex = middleIndex
+                } else {
+                    rightIndex = middleIndex - 1
+                }
             }
+
+            // если у нас первый элемент (который при бинарном поиске должен быть правильным ответом)
+            // больше либо равен минимальной границе, то в нашем массиве элементов, меньших минимума, нет
+            val excludedNumbersLeft = if (numbers[leftIndex] >= minNumber) 0 else leftIndex + 1
+
+            // находим все элементы, которые больше максимума
+            leftIndex = 0
+            rightIndex = numbers.size - 1
+
+            while (leftIndex < rightIndex) {
+                val middleIndex = (leftIndex + rightIndex) / 2
+
+                if (numbers[middleIndex] > maxNumber) {
+                    rightIndex = middleIndex
+                } else {
+                    leftIndex = middleIndex + 1
+                }
+            }
+            // если у нас последний элемент (который при бинарном поиске должен быть правильным ответом)
+            // меньше либо равен максимальной границе, то в нашем массиве элементов, больших максимума, нет
+            val excludedNumbersRight = if (numbers[leftIndex] <= maxNumber) 0 else numbers.size-leftIndex
+
+            excludedNumbers=excludedNumbersLeft+excludedNumbersRight
         }
 
-//        leftIndex = 0
-//        rightIndex = numbers.size - 1
-//
-//        while (leftIndex < rightIndex) {
-//            val middleIndex = (leftIndex + rightIndex + 1) / 2
-//
-//            if (numbers[middleIndex] <= maxNumber) {
-//                leftIndex = middleIndex
-//            } else {
-//                rightIndex = middleIndex - 1
-//                entriesExcluded++
-//            }
-//        }
-
-        val entries = numbers.size - entriesExcluded
+        val entries = numbers.size - excludedNumbers
 
         answers.add(entries)
     }
--- src/main/kotlin/season_5/block_4/B_LineWarships.kt
+++ src/main/kotlin/season_5/block_4/B_LineWarships.kt
@@ -1,23 +1,54 @@
 package org.example.season_5.block_4
 
+import java.math.BigInteger
+
 fun lineWarships() {
+    val cellsAmount = readln().toLong()
 
+    val begin = System.currentTimeMillis()
 
-    val k = readln().toInt()
+    var maxShipSizeLeft = 0L
+    var maxShipSizeRight = cellsAmount
 
-    var sum = 0
+    while (maxShipSizeLeft < maxShipSizeRight) {
+        val maxShipSizeMiddle = (maxShipSizeLeft + maxShipSizeRight + 1) / 2
 
-    (1..k).forEach { number ->
-
-        val index = number - 1
-
-        val oppositeNumber = k-index
-        val stepSum = number * oppositeNumber+index
-
-        sum += stepSum
+        if (countCellsByShipMaxSize(maxShipSizeMiddle) <= cellsAmount.toBigInteger()) {
+            maxShipSizeLeft = maxShipSizeMiddle
+        } else {
+            maxShipSizeRight = maxShipSizeMiddle - 1
+        }
     }
 
-    sum+=k-1
+    println(maxShipSizeLeft)
 
-    println(sum)
+    val end = System.currentTimeMillis()
+
+    println("Execution time is ${end - begin} ms")
+}
+
+private fun countCellsByShipMaxSize(k: Long): BigInteger {
+    // (k-1)*k/2 -  количество свободных клеток в рамках группы кораблей
+    // k-1 -  количество свободных клеток между группами кораблей
+
+    val totalShipCellsAmount: BigInteger
+
+    if (k % 2 == 1L) {
+        val n = k / 2 + 1
+        totalShipCellsAmount =
+            n.toBigInteger() * (2 * n - 1).toBigInteger() * (2 * n + 1).toBigInteger() / 3.toBigInteger();
+    } else {
+        val n = k / 2
+        totalShipCellsAmount =
+            2.toBigInteger() * n.toBigInteger() * (n + 1).toBigInteger() * (2 * n + 1).toBigInteger() / 3.toBigInteger();
+    }
+
+    val sum =
+        totalShipCellsAmount + k.toBigInteger() * (k - 1).toBigInteger() / 2.toBigInteger() + (k - 1).toBigInteger()
+    return sum
+}
+
+fun squareSum135(k: Int): BigInteger {
+    val maxNumberInSequence = 2 * k - 1
+    return k.toBigInteger() * (2 * maxNumberInSequence - 1).toBigInteger() * (2 * maxNumberInSequence + 1).toBigInteger() / 3.toBigInteger()
 }
